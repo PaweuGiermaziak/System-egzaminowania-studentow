4062de687c62b796c9c40a8d65c47d63e6cea464
iKamil5s <32705132+iKamil5s@users.noreply.github.com>
2017-11-22 12:46:55 +0100

    Delete main.js

--- INDEX/js/main.js
+++
@@ -1,6 +0,0 @@
-function dirTree(vr){
-	if(document.getElementById("ul"+vr).style.display != "block")
-		document.getElementById("ul"+vr).style.display = "block";
-	else
-		document.getElementById("ul"+vr).style.display = "none";
-}
